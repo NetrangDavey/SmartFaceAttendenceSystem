8da1db9c7185ba71f69b8ded89036335f9b0e365
Netrang <netrang.v@gmail.com>
2024-10-16 13:06:14 +0530

    Done styling and tested on phone

--- Frontend/Attendence_Frontend/vite.config.js
+++ Frontend/Attendence_Frontend/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Allows access from external devices
-    port: 5173, // Default Vite port
-  }
+ 
 })
